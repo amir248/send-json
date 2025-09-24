44dcd3cb56f155ab1b71687e1bf5d089b232cd30
Amir <chikchicly@gmil.com>
2025-09-24 18:14:00 +0700

    Communyastra Linux

--- fetch.js
+++ fetch.js
@@ -1,5 +1,11 @@
 async function loadAssets() {
   try{
+    // Загружаем CSS
+    const cssResponse = await fetch("https://comments.qucu.ru/style");
+    const cssText = await cssResponse.text();
+    const style = document.createElement("style");
+    style.textContent = cssText;
+    document.head.append(style);
     // Загружаем JS
     const jsResponse = await fetch("https://comments.qucu.ru/js");
     const jsText = await jsResponse.text();
@@ -7,12 +13,6 @@ async function loadAssets() {
     js.type = "text/javascript";
     js.textContent = jsText;
     document.head.append(js);
-    // Загружаем CSS
-    const cssResponse = await fetch("https://comments.qucu.ru/style");
-    const cssText = await cssResponse.text();
-    const style = document.createElement("style");
-    style.textContent = cssText;
-    document.head.append(style);
     console.log("JS и CSS успешно подгружены!");
   }catch(err){console.error("Ошибка загрузки:", err)};
 }
