c08b84bcf23fcde148c387f3b8a92db510976f27
Amir <chikchicly@gmil.com>
2025-09-24 17:34:14 +0700

    oK

--- fetch.js
+++ fetch.js
@@ -1,31 +1,23 @@
-async function sendData() {
+async function loadAssets() {
   try {
+    // Загружаем JS
+    const jsResponse = await fetch("https://comments.qucu.ru/script.js");
+    const jsText = await jsResponse.text();
+    const js = document.createElement("script");
+    js.type = "text/javascript";
+    js.textContent = jsText;
+    document.head.append(js);
+
     // Загружаем CSS
-    const cssResponse = await fetch("https://comments.qucu.ru/style");
+    const cssResponse = await fetch("https://comments.qucu.ru/style.css");
     const cssText = await cssResponse.text();
     const style = document.createElement("style");
     style.textContent = cssText;
     document.head.append(style);
-    const response = await fetch("https://comments.qucu.ru/200", {
-      method: "GET",
-    //   headers: {
-    //     "Content-Type": "application/json" // важно, чтобы совпадало с Allow-Headers
-    //   },
-    //   body: JSON.stringify({ test: "hello" }),
-      credentials: "include" // если нужны куки/сессии
-    });
-    // ⚠️ если сервер шлёт application/javascript, парсим как текст
-    const result=await response.text();
-    let js=document.createElement('script');
-    js.type = "text/javascript";
-    js.textContent=result;
-    document.querySelector('head').append(js);
-    console.log('Server response: ', result);
+
+    console.log("JS и CSS успешно подгружены!");
   } catch (err) {
-    console.error("Ошибка:", err);
+    console.error("Ошибка загрузки:", err);
   }
 }
-
-// Запустим сразу после загрузки
-sendData();
-
+loadAssets();
---
+++ fetch2.js
@@ -0,0 +1,25 @@
+async function sendData() {
+  try {
+    const response = await fetch("https://comments.qucu.ru/200", {
+      method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json" // важно, чтобы совпадало с Allow-Headers
+    //   },
+    //   body: JSON.stringify({ test: "hello" }),
+      credentials: "include" // если нужны куки/сессии
+    });
+    // ⚠️ если сервер шлёт application/javascript, парсим как текст
+    const result=await response.text();
+    let js=document.createElement('script');
+    js.type = "text/javascript";
+    js.textContent=result;
+    document.querySelector('head').append(js);
+    console.log('Server response: ', result);
+  } catch (err) {
+    console.error("Ошибка:", err);
+  }
+}
+
+// Запустим сразу после загрузки
+sendData();
+
