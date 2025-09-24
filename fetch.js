2e7b054f0ec3833d5b27d8f5c150104822cd1ea6
Amir <chikchicly@gmil.com>
2025-09-24 18:12:09 +0700

    oK

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 async function loadAssets() {
-  try {
+  try{
     // Загружаем JS
     const jsResponse = await fetch("https://comments.qucu.ru/js");
     const jsText = await jsResponse.text();
@@ -7,17 +7,13 @@ async function loadAssets() {
     js.type = "text/javascript";
     js.textContent = jsText;
     document.head.append(js);
-
     // Загружаем CSS
     const cssResponse = await fetch("https://comments.qucu.ru/style");
     const cssText = await cssResponse.text();
     const style = document.createElement("style");
     style.textContent = cssText;
     document.head.append(style);
-
     console.log("JS и CSS успешно подгружены!");
-  } catch (err) {
-    console.error("Ошибка загрузки:", err);
-  }
+  }catch(err){console.error("Ошибка загрузки:", err)};
 }
 loadAssets();
