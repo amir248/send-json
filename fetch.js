e1f850f77d7db5ba35e341274a2599fa14cac587
Amir <chikchicly@gmil.com>
2025-09-24 17:10:24 +0700

    Final Fantasy

--- fetch.js
+++ fetch.js
@@ -2,15 +2,16 @@ async function sendData() {
   try {
     const response = await fetch("https://comments.qucu.ru/200", {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json" // важно, чтобы совпадало с Allow-Headers
-      },
+    //   headers: {
+    //     "Content-Type": "application/json" // важно, чтобы совпадало с Allow-Headers
+    //   },
     //   body: JSON.stringify({ test: "hello" }),
       credentials: "include" // если нужны куки/сессии
     });
     // ⚠️ если сервер шлёт application/javascript, парсим как текст
     const result=await response.text();
     let js=document.createElement('script');
+    js.type = "text/javascript";
     js.textContent=result;
     document.querySelector('head').append(js);
     console.log('Server response: ', result);
